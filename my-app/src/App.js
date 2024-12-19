c4ef3d34d6dbd262c396f0363d8e3202f2370414
sunshine13067 <thesunshine.201606@example.com>
2024-12-20 00:08:10 +0200

    complete home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+ 
+import Home from "./components/Home/Home"; 
+import Preloader from "../src/components/Pre";
+import Footer from "./components/Footer";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+
+ import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+            <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+           <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
